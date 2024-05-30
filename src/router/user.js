9fe61177f819b279bfa8a399d64cdf9572fc487e
Daniel Buitrago <danielb.alzate1@gmail.com>
2024-05-30 14:41:21 -0500

    Add methods to update, delete and find specific user

--- src/router/user.js
+++ src/router/user.js
@@ -24,4 +24,38 @@ router.get('/users', (req, res) => {
     });
 });
 
+//Get specific users
+router.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
+
+//Update specific users
+router.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, email, age, phone, country } = req.body;
+  userSchema
+    .updateOne({ _id: id }, { $set: { name, email, age, phone, country } })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
+
+//Delete specific users
+router.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .deleteOne({ _id: id })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
+
 export default router;
